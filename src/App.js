aebad652fd06742454b7fab30f8f10ad29e8349e
Rio <rogerio.taxlien@gmail.com>
2021-07-27 00:07:18 -0500

    creating project one

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
 import './App.css';
+import { boys } from './boysArray';
+import { girls } from './girlsArray';
+
+const getGirlName = () => girls[Math.floor(Math.random() * girls.length)];
+const getBoyName = () => boys[Math.floor(Math.random() * boys.length)];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <h1>Random Baby Name Generator</h1>
       </header>
+      <body className='App-body'>
+        <div
+          id='babyName'
+          style={{
+            backgroundColor: 'whitesmoke',
+            marginBottom: 30,
+            borderRadius: 8,
+            textAlign: 'center',
+            width: 500,
+            height: 200,
+            fontSize: 32,
+            fontFamily: 'roboto',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        ></div>
+        <div style={{ flexDirection: 'row' }}>
+          <button
+            style={{
+              padding: 100,
+              backgroundColor: 'blue',
+              marginRight: 30,
+              borderRadius: 8,
+              fontFamily: 'roboto',
+              fontSize: 32,
+              color: 'pink',
+              borderColor: 'white',
+              borderWidth: 3,
+            }}
+            onClick={() => {
+              const firstName = getBoyName();
+              let middleName = getBoyName();
+              document.getElementById(
+                'babyName'
+              ).innerHTML = `${firstName} ${middleName}`;
+            }}
+          >
+            Boy
+          </button>
+          <button
+            style={{
+              padding: 100,
+              backgroundColor: 'pink',
+              borderRadius: 8,
+              fontFamily: 'roboto',
+              fontSize: 32,
+              borderColor: 'white',
+              borderWidth: 3,
+              color: 'blue',
+            }}
+            onClick={() => {
+              const firstName = getGirlName();
+              let middleName = getGirlName();
+              document.getElementById(
+                'babyName'
+              ).innerHTML = `${firstName} ${middleName}`;
+            }}
+          >
+            Girl
+          </button>
+        </div>
+      </body>
     </div>
   );
 }
